e69174c2a0d16810ae471090fa255f3ddc41a972
huonchan <68013384+huonchan@users.noreply.github.com>
2024-11-03 11:42:01 +0900

    htmlジェネレーター仮生成

---
+++ html_creater/templates.js
@@ -0,0 +1,42 @@
+const DUMMY_TEXT= `Dummy`;
+
+const HTML_TEMP = 
+`
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="UTF-8">
+  <title>${DUMMY_TEXT}</title>
+</head>
+
+<body>
+
+</body>
+</html>
+`;
+
+
+const HTML_DIV = 
+`
+<div id="dummy">
+
+</div>
+`;
+
+const HTML_TEXTAREA = 
+`
+<textarea id="" , rows="5" cols="40">悪ポテトのたち</textarea>
+`;
+
+const HTML_BUTTON = 
+`
+<button onclick="dummy_function()">ボタン</button>
+`;
+
+
+const HTML_CHECKBOX = 
+`
+<input type="checkbox" id="checkbox" name="checkbox" value="send_param" , checked >
+<label for="checkbox">check</label>
+`;
